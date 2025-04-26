5361fcd5d0496fc785114af45ea1c4cb42ac4c31
jimxl <tianxiaxl@gmail.com>
2025-04-26 23:12:43 +0800

    添加了页面的自动刷新

--- app/routes/types/home.ts
+++
@@ -1,6 +0,0 @@
-export namespace Route {
-  export interface MetaArgs {
-    [key: string]: any;
-  }
-  export type LoaderArgs = any;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
+    watch: {
+      usePolling: true,
+      interval: 100
+    },
     proxy: {
       '/api': {
         target: 'http://192.168.2.174:8888',
